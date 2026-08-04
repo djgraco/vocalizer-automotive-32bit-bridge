5c5004c26f398ba776c4e32cf7b4aa1620a543eb
DJ Graco <nawrocki.gracjan7@gmail.com>
2026-08-04 12:45:47 +0200

    Restore missing voices download prompt

--- tests/test_menu_runtime.py
+++ tests/test_menu_runtime.py
@@ -98,10 +98,24 @@ class MenuRuntimeTests(unittest.TestCase):
 		wx.OK = 1
 		wx.ICON_ERROR = 2
 		wx.ICON_INFORMATION = 4
+		wx.ICON_WARNING = 32
 		wx.YES = 8
 		wx.NO = 16
 		wx.YES_NO = wx.YES | wx.NO
 		wx.Menu = FakeMenu
+		wx.callLaterInstances = []
+
+		class FakeCallLater:
+			def __init__(self, delay, callback):
+				self.delay = delay
+				self.callback = callback
+				self.stopped = False
+				wx.callLaterInstances.append(self)
+
+			def Stop(self):
+				self.stopped = True
+
+		wx.CallLater = FakeCallLater
 
 		package = types.ModuleType(packageName)
 		package.__path__ = [str(SOURCE_PATH.parent)]
@@ -148,6 +162,70 @@ class MenuRuntimeTests(unittest.TestCase):
 			self.assertFalse(oldMenu.destroyed)
 			self.assertIsNot(plugin.submenu_vocalizer, oldMenu)
 			self.assertIsNotNone(plugin.menuItem)
+		finally:
+			for name in moduleNames:
+				oldModule = previousModules[name]
+				if oldModule is None:
+					sys.modules.pop(name, None)
+				else:
+					sys.modules[name] = oldModule
+			if previousTranslation is None:
+				builtins.__dict__.pop("_", None)
+			else:
+				builtins._ = previousTranslation
+
+	def test_missing_voices_warning_keeps_menu_and_offers_download(self):
+		module, sysTrayIcon, previousModules, moduleNames, previousTranslation = (
+			self._load_plugin()
+		)
+		try:
+			module.getLicenseInfo = lambda: "none"
+			module.getDefaultLicensePath = lambda: "missing-license.ini"
+			messageBoxes = []
+			module.gui.messageBox = lambda *args: messageBoxes.append(args) or module.wx.YES
+			openedDownloads = []
+
+			plugin = module.GlobalPlugin()
+			plugin._openVoicesDownload = lambda: openedDownloads.append(True)
+
+			self.assertIsNotNone(plugin.menuItem)
+			self.assertEqual(len(module.wx.callLaterInstances), 1)
+			warning = module.wx.callLaterInstances[0]
+			self.assertEqual(warning.delay, 2000)
+			self.assertIs(plugin._noVoicesWarning, warning)
+
+			warning.callback()
+
+			self.assertIsNone(plugin._noVoicesWarning)
+			self.assertEqual(len(messageBoxes), 1)
+			self.assertEqual(openedDownloads, [True])
+		finally:
+			for name in moduleNames:
+				oldModule = previousModules[name]
+				if oldModule is None:
+					sys.modules.pop(name, None)
+				else:
+					sys.modules[name] = oldModule
+			if previousTranslation is None:
+				builtins.__dict__.pop("_", None)
+			else:
+				builtins._ = previousTranslation
+
+	def test_missing_voices_warning_is_cancelled_during_termination(self):
+		module, sysTrayIcon, previousModules, moduleNames, previousTranslation = (
+			self._load_plugin()
+		)
+		try:
+			module.getLicenseInfo = lambda: "none"
+			module.getDefaultLicensePath = lambda: "missing-license.ini"
+			plugin = module.GlobalPlugin()
+			warning = plugin._noVoicesWarning
+
+			plugin.terminate()
+
+			self.assertTrue(plugin._terminating)
+			self.assertTrue(warning.stopped)
+			self.assertIsNone(plugin._noVoicesWarning)
 		finally:
 			for name in moduleNames:
 				oldModule = previousModules[name]
